a7c43644c691af0ec31657a2c0c963ea0f73a21c
Eric <ericpeterson@tamu.edu>
2024-05-21 22:57:20 -0500

    4.2

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-require('dotenv').config()
-
-//const logger = require('./utils/logger')
-//const {MONGO_URI, PORT} = require('./utils/config')
+// const logger = require('./utils/logger')
+const config = require('./utils/config')
 const express = require('express')
 const blogRouter = require('./controllers/blogs')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog.js')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
 
 
+const mongoUrl = config.MONGO_URI
+mongoose.connect(mongoUrl)
+  .then(() => console.log("Connected to database"))
 
 app.use(cors())
 app.use(express.json())
-
 
 app.use('/api/blogs', blogRouter)
 
 
 module.exports = app
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
