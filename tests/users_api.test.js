652989ba19d4b8165b9d68c800dd7be9a34ec349
Eric <ericpeterson@tamu.edu>
2024-05-25 12:41:32 -0500

    4.16 tests

--- tests/users_api.test.js
+++ tests/users_api.test.js
@@ -7,27 +7,65 @@ const api = supertest(app)
 
 const User = require('../models/user')
 
+const badUsername = {
+    "name": "Eric4",
+    "username": "Er",
+    "password": "testingpassword"
+}
+
+const badPassword = {
+    "name": "Eric5",
+    "username": "Eric5",
+    "password": "t"
+}
+
+const usedUsername = {
+    "name": "Eric2",
+    "username": "ZGong1",
+    "password": "testingpassword"
+}
+
 beforeEach(async () => {
     await User.deleteMany({})
 
     var newUser = new User({
         "name": "Eric1",
         "username": "ZGong1",
-        "password": "testingpassword"
+        "passwordHash": "testingpassword"
     })
     await newUser.save()
 
     newUser = new User({
         "name": "Eric2",
         "username": "ZGong2",
-        "password": "testingpassword"
+        "passwordHash": "testingpassword"
     })
     await newUser.save()
 
     newUser = new User({
         "name": "Eric3",
         "username": "ZGong3",
-        "password": "testingpassword"
+        "passwordHash": "testingpassword"
     })
     await newUser.save()
+})
+
+test('too short username', async () => {
+    const response = await api.post('/api/users').send(badUsername).expect(400)
+    assert.deepStrictEqual(response.body, {error: "invalid username length"})
+})
+
+test('too short password', async () => {
+    const response = await api.post('/api/users').send(badPassword).expect(400)
+    assert.deepStrictEqual(response.body, {error: "invalid password length"})
+})
+
+test('username already in use', async () => {
+    const response = await api.post('/api/users').send(usedUsername).expect(400)
+    assert.deepStrictEqual(response.body, {error: "username is already taken"})
+})
+
+
+after(async () => {
+    await mongoose.connection.close()
 })
